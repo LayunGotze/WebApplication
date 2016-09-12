20763c1b655048619265790961064879cb7d15b6
LayunGotze <1727176361@qq.com>
2016-09-12 22:36:13 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>无标题文档</title>
+</head>
+
+<body>
+<p>
+  <?php 
+	echo"a login page";
+?>
+</p>
+<p> 
+  <label for="Username">Username:</label>
+  <input type="text" name="Username" id="Username">
+</p>
+<p>&nbsp;</p>
+<p>
+  <label for="password">Password:</label>
+  <input type="password" name="password" id="password">
+</p>
+<p>&nbsp;</p>
+<p>
+  <input type="button" name="login" value="login">
+    </p>
+<p>&nbsp;</p>
+<p><a href="#">forgot your password?</a></p>
+</body>
+</html>
